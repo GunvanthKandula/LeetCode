26c3aa0e28f61a47882f73ffacec5ef05b552cb7
kandula gunvanth <gunvanthkandula@gmail.com>
2026-05-26 13:32:29 +0530

    Time: 1 ms (99.95%) | Memory: 77.2 MB (64.85%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,7 +5,7 @@ class Solution {
        for(int i = 0; i < nums.length;i++){
         sum = sum + nums[i];
         if(sum>maxi){
-            maxi = Math.max(sum,maxi);
+            maxi = sum;
         }
         if(sum<0){
             sum = 0;
