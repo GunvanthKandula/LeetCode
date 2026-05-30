fc71e6b9d961926e51eaec055971eb7a4bb91cb5
kandula gunvanth <gunvanthkandula@gmail.com>
2026-05-30 23:07:48 +0530

    Time: 1 ms (87.54%) | Memory: 45.3 MB (77.73%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,20 +2,20 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        recurPermute(nums, ds, ans, freq);
+        boolean[] freq = new boolean[nums.length];
+        recurpermute(nums, ans, ds, freq);
         return ans;
     }
-     private void recurPermute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean []freq) {
-        if(ds.size() == nums.length) {
+    public void recurpermute(int[] nums, List<List<Integer>> ans, List<Integer> ds, boolean[] freq){
+        if(ds.size() == nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i = 0;i<nums.length;i++) {
-            if(!freq[i]) {
+        for(int i = 0;i < nums.length;i++){
+            if(!freq[i]){
                 freq[i] = true;
                 ds.add(nums[i]);
-                recurPermute(nums, ds, ans, freq);
+                recurpermute(nums, ans, ds, freq);
                 ds.remove(ds.size() - 1);
                 freq[i] = false;
             }
