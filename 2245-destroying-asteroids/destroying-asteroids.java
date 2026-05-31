d9cb5297c704e6c7026439c1a86e4562603d0968
kandula gunvanth <gunvanthkandula@gmail.com>
2026-05-31 22:21:25 +0530

    Time: 26 ms (81.52%) | Memory: 107.4 MB (47.81%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean asteroidsDestroyed(long mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        for(int i = 0;i < asteroids.length;i++){
+              if(mass >= asteroids[i])
+              {
+                mass += asteroids[i];
+              }
+              else
+              {
+                return false;
+              }
+        }
+        return true;
+    }
+}
