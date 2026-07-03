bf03248e9243becfe5607b3c51ece1d3d6c662b7
kandula gunvanth <gunvanthkandula@gmail.com>
2026-07-03 12:43:58 +0530

    Time: 12 ms (28.05%) | Memory: 50.3 MB (36.75%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,22 +1,23 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int n = weights.length;
-        long low =(long)max(weights);
-        long high = (long)totalSum(weights);
-        int result = 0;
+        long low =0;
+        long high = 0;
+        for(int i = 0;i<n;i++){
+            low = Math.max(low,weights[i]);
+            high+=weights[i];
+        }
         while(low<=high){
             long mid = (low+high)/2;
-            int ans = howManyDays(weights,(int)mid);
-            if(ans<=days){
-                result = (int)mid;
+            int noofdays = howManyDays(weights,(int)mid);
+            if(noofdays<=days){
                 high = mid - 1;
             }
-            else if(ans>days){
+            else{
                 low = mid +1;
-            }
-            
+            }   
         }
-        return result;
+        return (int)low;
     }
     public int howManyDays(int[] weights,int capacity){
         int load = 0;
@@ -29,19 +30,5 @@ class Solution {
            load+=weights[i];
         }
         return days;
-    }
-    public int max(int[] weights){
-        int maxi = 0;
-        for(int i =0;i<weights.length;i++){
-            maxi=Math.max(maxi,weights[i]);
-        }
-        return maxi;
-    }
-    public int totalSum(int[] weights){
-        int result = 0;
-        for(int i : weights){
-            result +=i;
-        }
-        return result;
     }
 }
