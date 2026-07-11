5f08d0113049ae08f5700346bbda73c70dc89cb6
kandula gunvanth <gunvanthkandula@gmail.com>
2026-07-11 19:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (62.03%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -5,38 +5,19 @@ class Solution {
         if(target>matrix[m-1][n-1])return false;
         if(target<matrix[0][0]) return false;
         int low = 0;
-        int high = m-1;
+        int high = m*n-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(searchtarget(matrix[mid],target)){
+            if(matrix[mid/n][mid%n]==target){
                 return true;
             }
-            else if(matrix[mid][0]>target){
+            else if(matrix[mid/n][mid%n]>target){
                 high = mid-1;
             }
-            else if(matrix[mid][0]<target){
+            else{
                 low=mid+1;
             }
         }    
-        return false;
-    }
-    public boolean searchtarget(int[] row,int target){
-        if(target>row[row.length-1]) return false;
-        if(target<row[0]) return false;
-        int low = 0;
-        int high = row.length-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(row[mid]==target){
-                return true;
-            }
-            else if(target>row[mid]){
-                low=mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
         return false;
     }
 }
